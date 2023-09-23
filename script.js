9ce7b24b51812685d54481db3140c6c9e82827d2
theharlen <32777370+theharlen@users.noreply.github.com>
2023-09-23 14:11:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function verificarResposta() {
+    var inputResposta = document.getElementById("resposta");
+    var divRespostaCorreta = document.getElementById("respostaCorreta");
+    var botaoVerificar = document.getElementById("botaoVerificar");
+    var tituloPergunta = document.getElementById("tituloPergunta");
+    var imagemResposta = document.getElementById("imagemResposta"); // Obtemos a referência da imagem
+    
+    var resposta = inputResposta.value;
+    
+    if(resposta.trim().toLowerCase() === "paris") {
+        tocarAudio();
+        divRespostaCorreta.innerHTML = "Certa Resposta!<br>" + resposta.toUpperCase();
+
+        // Definimos a fonte da imagem e a tornamos visível
+        imagemResposta.src = 'G:/My Drive/Programação/JavaScript/QuizzBeta002/cupom.jpg';
+        imagemResposta.style.display = "block";
+        
+        divRespostaCorreta.style.display = "block";
+        botaoVerificar.style.display = "none";
+        tituloPergunta.style.display = "none";
+        inputResposta.style.display = "none";
+    } else {
+        alert("Resposta incorreta. Tente novamente.");
+        divRespostaCorreta.style.display = "none";
+    }
+}
+
+function tocarAudio() {
+    var audio = new Audio('G:/My Drive/Programação/JavaScript/QuizzBeta002/certa.mp3');
+    audio.play();
+}
